4e8893dc51d0cd9622925f12bf73787f370e2b2e
Bses <nickmanandhar16@gmail.com>
2025-05-01 11:37:05 +0545

    Strating API Integration

---
+++ app/src/main/java/com/bses/assignmentapp/Model/ApiService.java
@@ -0,0 +1,11 @@
+package com.bses.assignmentapp.Model;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @POST("api/user/login")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
+
+}
---
+++ app/src/main/java/com/bses/assignmentapp/Model/User.java
@@ -0,0 +1,12 @@
+package com.bses.assignmentapp.Model;
+
+public class User {
+    int id;
+    String name;
+    String email;
+
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
